ecdbb75dfb8e43599a917f48939ea1ab430c08b0
iptaylortechnical <iptaylortechnical@gmail.com>
2015-10-10 10:35:28 -0600

    commited changes to app.js

--- legacy/app.js
+++ legacy/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-
+it
 //Advisor
 //advisor registration
 var advReg = require('./routes/advReg');
