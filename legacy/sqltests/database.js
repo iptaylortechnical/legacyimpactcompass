54ec0cfffaf54091ac3125404c3c773a36db59d7
iptaylortechnical <iptaylortechnical@gmail.com>
2015-10-10 11:11:56 -0600

    working test

--- legacy/sqltests/database.js
+++ legacy/sqltests/database.js
@@ -1,34 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
 var connectionString = 'postgres://localhost:5432';
 
 router.get('/', function(req, res, next){
+	var results = [];
+
+	    // Grab data from http request
 	pg.connect(connectionString, function(err, client, done) {
-		// Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({ success: false, data: err});
-    }
 
-    // SQL Query > Insert Data
-    client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+//     // SQL Query > Insert Data
+     client.query("INSERT INTO items(text, complete) values('there', '0')");
 
-    // SQL Query > Select Data
-    var query = client.query("SELECT * FROM items ORDER BY id ASC");
-
-    // Stream results back one row at a time
+//     // SQL Query > Select Data
+     var query = client.query("SELECT * FROM items ORDER BY id ASC");
+//
+//     // Stream results back one row at a time
     query.on('row', function(row) {
         results.push(row);
     });
-
-    // After all data is returned, close connection and return results
+//
+//     // After all data is returned, close connection and return results
     query.on('end', function() {
         done();
         return res.json(results);
     });
 	});
-})
+});
 
 module.exports = router;
