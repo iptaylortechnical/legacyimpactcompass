a3880357c28d3fc6f018ca596d80ab26defa2b75
iptaylortechnical <iptaylortechnical@gmail.com>
2015-10-16 18:57:04 -0600

    register redirection if user exists

--- legacy/routes/usrReg.js
+++ legacy/routes/usrReg.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../utilities/auth');
 
 router.get('/', function(req, res, next){
-	res.render('main');
+	session = req.cookies.sessionID || '';
+	
+	auth.isUser(session, function(good){
+		if(!good){
+			res.render('main');
+		}else{
+			res.writeHead(302, {
+			  'Location': '/'
+			  //add other headers here...
+			});
+			res.end();
+		}
+	});
 })
 
 module.exports = router;
