bf1d60861b0a14ec7b39b07485ecc15925a0c89a
iptaylortechnical <iptaylortechnical@gmail.com>
2015-10-19 13:03:32 -0600

    removed _temp flag from advReg view reference

--- legacy/routes/advReg.js
+++ legacy/routes/advReg.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-	res.render('advReg_temp');
+	res.render('advReg');
 });
 
 module.exports = router;
