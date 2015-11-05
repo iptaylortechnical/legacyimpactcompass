573b97196980e8c36abfbacbc71b117f1a49cb0a
iptaylortechnical <iptaylortechnical@gmail.com>
2015-11-04 19:24:46 -0700

    tests

--- legacy/routes/home.js
+++ legacy/routes/home.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../utilities/auth');
 
 router.post('/', function(req, res){
 	res.send('get only');
 });
 
 router.get('/', function(req, res){
-	res.render('advisor');
+	
+	session = req.cookies.sessionID || '';
+	
+	auth.isUser(session, function(good){
+		if(good){
+			console.log('yep');
+			res.advisor('advisor');
+		}else{
+			auth.isAdvisor(session, function(good){
+				if(good){
+					res.writeHead(302, {
+					  'Location': 'advisor'
+					  //add other headers here...
+					});
+					res.end();
+				}else{
+					res.writeHead(302, {
+					  'Location': 'login'
+					  //add other headers here...
+					});
+					res.end();
+				}
+			});
+		}
+	});
 })
 
 module.exports = router;
