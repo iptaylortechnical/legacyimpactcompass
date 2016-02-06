35cfbb2f6836519e6ca126d968a4012a8242975e
iptaylortechnical <iptaylortechnical@gmail.com>
2016-02-05 21:49:15 -0700

    rollback

--- legacy/socket/hier.js
+++ legacy/socket/hier.js
@@ -1,260 +1,14 @@
-var answerBuilder = {
-	"how-many-children": function(answer, input){
-		var stuff = [
-			[
-				{
-					"qid": "name-child-0",
-					"title": "What is the name of you first child?",
-					"description": "Please enter the name of you first child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				},
-				{
-					"qid": "name-child-1",
-					"title": "What is the name of your second child?",
-					"description": "Please enter the name of your second child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				}
-			],
-			[
-				{
-					"qid": "name-child-0",
-					"title": "What is the name of you first child?",
-					"description": "Please enter the name of you first child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				},
-				{
-					"qid": "name-child-1",
-					"title": "What is the name of your second child?",
-					"description": "Please enter the name of your second child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				},
-				{
-					"qid": "name-child-2",
-					"title": "What is the name of your third child?",
-					"description": "Please enter the name of your third child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				}
-			],
-			[
-				{
-					"qid": "name-child-0",
-					"title": "What is the name of you first child?",
-					"description": "Please enter the name of you first child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				},
-				{
-					"qid": "name-child-1",
-					"title": "What is the name of your second child?",
-					"description": "Please enter the name of your second child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				},
-				{
-					"qid": "name-child-2",
-					"title": "What is the name of your third child?",
-					"description": "Please enter the name of your third child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				},
-				{
-					"qid": "name-child-3",
-					"title": "What is the name of your fourth child?",
-					"description": "Please enter the name of your fourth child.",
-					"answers": [
-						{
-							"options": "input",
-							"offspring": {
-								"hasChildren": false
-							}
-						}
-					]
-				}
-			]
-		];
-		
-		if(input){
-			var ret = [];
-			
-			var proto = {
-				"qid": "name-child-",
-				"title": "What is the name of child ",
-				"description": "Please enter the name of child ",
-				"answers": [
-					{
-						"options": "input",
-						"offspring": {
-							"hasChildren": false
-						}
-					}
-				]
-			};
-			
-			for(var i = 0; i < answer; i++){
-				proto.qid = proto.qid + i;
-				proto.title = proto.title + i;
-				proto.description = proto.description + i;
-				
-				ret.push(proto);
-			}
-			
-			return ret;
-		}else{
-			return stuff[answer];
-		}
-	}
-}
-
-
-
-var dataNew = [
-	{
-		"qid": "name",
-		"title": "What is your name?",
-		"description": "First and last is fine.",
-		"answer": [
-			{
-				"options": "input",
-				"offspring": {
-					"hasChildren": false
-				}
-			}
-		]
-	},
-	{
-		"qid": "how-many-children",
-		"title": "How many children do you have?",
-		"Please select the number of children you have, or other.",
-		"answer": [
-			{
-				"description": "2 children",
-				"answer": 2,
-				"offspring": {
-					"hasChildren": true,
-				}
-			},
-			{
-				"description": "3 children",
-				"answer": 3,
-				"offspring": {
-					"hasChildren": true,
-				}
-			},
-			{
-				"description": "4 children",
-				"answer": 4,
-				"offspring": {
-					"hasChildren": true,
-				}
-			},
-			{
-				"options": "input",
-				"offspring":{
-					"hasChildren": true,
-				}
-			}
-		]
-	}
-]
-
-// var data = {
-// 	"qid": "how-many-children",
-// 	"title": "How many children do you have?",
-// 	"description": "Please select the number of children you have.",
-// 	"answers": [
-// 		{
-// 			"description": "5 children",
-// 			"answer": 5,
-// 			"offspring": {
-// 				"hasChildren": true,
-// 				"childCount": 2
-// 			},
-// 			"children": [
+// var answerBuilder = {
+// 	"how-many-children": function(answer, input){
+// 		var stuff = [
+// 			[
 // 				{
-// 					"qid": "kind-of-tacos",
-// 					"title": "What kind of tacos?",
-// 					"description": "What kind of tacos do you prefer?",
+// 					"qid": "name-child-0",
+// 					"title": "What is the name of you first child?",
+// 					"description": "Please enter the name of you first child.",
 // 					"answers": [
 // 						{
-// 							"description": "meat taco",
-// 							"answer": "meat",
-// 							"offspring": {
-// 								"hasChildren": true,
-// 								"childCount": 1
-// 							},
-// 							"children": [
-// 								{
-// 									"qid": "kind-of-meat",
-// 									"title": "What kind of meat?",
-// 									"description": "What kind of meat do you like on your sub?",
-// 									"answers": [
-// 										{
-// 											"description": "ham meat",
-// 											"answer": "ham",
-// 											"offspring": {
-// 												"hasChildren": false
-// 											}
-// 										}
-// 									]
-// 								}
-// 							]
-// 						},
-// 						{
-// 							"description": "veggie taco",
-// 							"answer": "veggie",
+// 							"options": "input",
 // 							"offspring": {
 // 								"hasChildren": false
 // 							}
@@ -262,20 +16,107 @@ var dataNew = [
 // 					]
 // 				},
 // 				{
-// 					"qid": "kind-of-subs",
-// 					"title": "What kind of subs?",
-// 					"description": "What kind of subway sandwhiches do they like?",
+// 					"qid": "name-child-1",
+// 					"title": "What is the name of your second child?",
+// 					"description": "Please enter the name of your second child.",
 // 					"answers": [
 // 						{
-// 							"description": "meatball sub sandwhich",
-// 							"answer": "meatball",
+// 							"options": "input",
 // 							"offspring": {
 // 								"hasChildren": false
 // 							}
-// 						},
+// 						}
+// 					]
+// 				}
+// 			],
+// 			[
+// 				{
+// 					"qid": "name-child-0",
+// 					"title": "What is the name of you first child?",
+// 					"description": "Please enter the name of you first child.",
+// 					"answers": [
 // 						{
-// 							"description": "chicken bacon ranch melt sub sandwhich",
-// 							"answer": "cbrm",
+// 							"options": "input",
+// 							"offspring": {
+// 								"hasChildren": false
+// 							}
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"qid": "name-child-1",
+// 					"title": "What is the name of your second child?",
+// 					"description": "Please enter the name of your second child.",
+// 					"answers": [
+// 						{
+// 							"options": "input",
+// 							"offspring": {
+// 								"hasChildren": false
+// 							}
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"qid": "name-child-2",
+// 					"title": "What is the name of your third child?",
+// 					"description": "Please enter the name of your third child.",
+// 					"answers": [
+// 						{
+// 							"options": "input",
+// 							"offspring": {
+// 								"hasChildren": false
+// 							}
+// 						}
+// 					]
+// 				}
+// 			],
+// 			[
+// 				{
+// 					"qid": "name-child-0",
+// 					"title": "What is the name of you first child?",
+// 					"description": "Please enter the name of you first child.",
+// 					"answers": [
+// 						{
+// 							"options": "input",
+// 							"offspring": {
+// 								"hasChildren": false
+// 							}
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"qid": "name-child-1",
+// 					"title": "What is the name of your second child?",
+// 					"description": "Please enter the name of your second child.",
+// 					"answers": [
+// 						{
+// 							"options": "input",
+// 							"offspring": {
+// 								"hasChildren": false
+// 							}
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"qid": "name-child-2",
+// 					"title": "What is the name of your third child?",
+// 					"description": "Please enter the name of your third child.",
+// 					"answers": [
+// 						{
+// 							"options": "input",
+// 							"offspring": {
+// 								"hasChildren": false
+// 							}
+// 						}
+// 					]
+// 				},
+// 				{
+// 					"qid": "name-child-3",
+// 					"title": "What is the name of your fourth child?",
+// 					"description": "Please enter the name of your fourth child.",
+// 					"answers": [
+// 						{
+// 							"options": "input",
 // 							"offspring": {
 // 								"hasChildren": false
 // 							}
@@ -283,30 +124,189 @@ var dataNew = [
 // 					]
 // 				}
 // 			]
-// 		},
-// 		{
-// 			"description": "6 children",
-// 			"answer": 6,
-// 			"offspring": {
-// 				"hasChildren": false
+// 		];
+//
+// 		if(input){
+// 			var ret = [];
+//
+// 			var proto = {
+// 				"qid": "name-child-",
+// 				"title": "What is the name of child ",
+// 				"description": "Please enter the name of child ",
+// 				"answers": [
+// 					{
+// 						"options": "input",
+// 						"offspring": {
+// 							"hasChildren": false
+// 						}
+// 					}
+// 				]
+// 			};
+//
+// 			for(var i = 0; i < answer; i++){
+// 				proto.qid = proto.qid + i;
+// 				proto.title = proto.title + i;
+// 				proto.description = proto.description + i;
+//
+// 				ret.push(proto);
 // 			}
-// 		},
-// 		{
-// 			"description": "7 children",
-// 			"answer": 7,
-// 			"offspring": {
-// 				"hasChildren": false
-// 			}
-// 		},
-// 		{
-// 			"description": "8 children",
-// 			"answer": 8,
-// 			"offspring": {
-// 				"hasChildren": false
-// 			}
+//
+// 			return ret;
+// 		}else{
+// 			return stuff[answer];
 // 		}
-// 	]
-// };
+// 	}
+// }
+//
+//
+//
+// var dataNew = [
+// 	{
+// 		"qid": "name",
+// 		"title": "What is your name?",
+// 		"description": "First and last is fine.",
+// 		"answer": [
+// 			{
+// 				"options": "input",
+// 				"offspring": {
+// 					"hasChildren": false
+// 				}
+// 			}
+// 		]
+// 	},
+// 	{
+// 		"qid": "how-many-children",
+// 		"title": "How many children do you have?",
+// 		"Please select the number of children you have, or other.",
+// 		"answer": [
+// 			{
+// 				"description": "2 children",
+// 				"answer": 2,
+// 				"offspring": {
+// 					"hasChildren": true,
+// 				}
+// 			},
+// 			{
+// 				"description": "3 children",
+// 				"answer": 3,
+// 				"offspring": {
+// 					"hasChildren": true,
+// 				}
+// 			},
+// 			{
+// 				"description": "4 children",
+// 				"answer": 4,
+// 				"offspring": {
+// 					"hasChildren": true,
+// 				}
+// 			},
+// 			{
+// 				"options": "input",
+// 				"offspring":{
+// 					"hasChildren": true,
+// 				}
+// 			}
+// 		]
+// 	 }
+// ]
+
+var data = {
+	"qid": "how-many-children",
+	"title": "How many children do you have?",
+	"description": "Please select the number of children you have.",
+	"answers": [
+		{
+			"description": "5 children",
+			"answer": 5,
+			"offspring": {
+				"hasChildren": true,
+				"childCount": 2
+			},
+			"children": [
+				{
+					"qid": "kind-of-tacos",
+					"title": "What kind of tacos?",
+					"description": "What kind of tacos do you prefer?",
+					"answers": [
+						{
+							"description": "meat taco",
+							"answer": "meat",
+							"offspring": {
+								"hasChildren": true,
+								"childCount": 1
+							},
+							"children": [
+								{
+									"qid": "kind-of-meat",
+									"title": "What kind of meat?",
+									"description": "What kind of meat do you like on your sub?",
+									"answers": [
+										{
+											"description": "ham meat",
+											"answer": "ham",
+											"offspring": {
+												"hasChildren": false
+											}
+										}
+									]
+								}
+							]
+						},
+						{
+							"description": "veggie taco",
+							"answer": "veggie",
+							"offspring": {
+								"hasChildren": false
+							}
+						}
+					]
+				},
+				{
+					"qid": "kind-of-subs",
+					"title": "What kind of subs?",
+					"description": "What kind of subway sandwhiches do they like?",
+					"answers": [
+						{
+							"description": "meatball sub sandwhich",
+							"answer": "meatball",
+							"offspring": {
+								"hasChildren": false
+							}
+						},
+						{
+							"description": "chicken bacon ranch melt sub sandwhich",
+							"answer": "cbrm",
+							"offspring": {
+								"hasChildren": false
+							}
+						}
+					]
+				}
+			]
+		},
+		{
+			"description": "6 children",
+			"answer": 6,
+			"offspring": {
+				"hasChildren": false
+			}
+		},
+		{
+			"description": "7 children",
+			"answer": 7,
+			"offspring": {
+				"hasChildren": false
+			}
+		},
+		{
+			"description": "8 children",
+			"answer": 8,
+			"offspring": {
+				"hasChildren": false
+			}
+		}
+	]
+};
 
 
 function getLocation(data, q) {
